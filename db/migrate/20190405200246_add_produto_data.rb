6d963c795f1f9d9ba468300d193ecbadb9674201
MatheusBMaia <matt.485@hotmail.com>
2019-04-06 00:34:56 -0300

    heroku commit

--- db/migrate/20190405200246_add_produto_data.rb
+++ db/migrate/20190405200246_add_produto_data.rb
@@ -1,37 +1,33 @@
 class AddProdutoData < ActiveRecord::Migration[5.2]
   def self.up
+    Grupo.create(
+         :nomegrupo => "Boneco Minions" )
        Produto.create(
-       	 :codigogrupo_id => 1,
          :codigoproduto_id => 1,
          :nomeproduto => "Boneco Minions Stuart",
          :descricaoproduto => "Boneco de Vinil Minions Stuart. Seu filho vai adorar o boneco do filme Meu Malvado Favorito, com braços articulados e cintura giratória. A diversão estará garantida.",
          :valorunitario => 68.90 )
        Produto.create(
-       	 :codigogrupo_id => 1,
          :codigoproduto_id => 2,
          :nomeproduto => "Boneco Minions Bob",
          :descricaoproduto => "Boneco de Vinil Minions Bob. Seu filho vai adorar o boneco do filme Meu Malvado Favorito, com braços articulados e cintura giratória. A diversão estará garantida.",
          :valorunitario => 68.90 )
        Produto.create(
-       	 :codigogrupo_id => 1,
          :codigoproduto_id => 3,
          :nomeproduto => "Boneco Minions Kevin",
          :descricaoproduto => "Boneco de Vinil Minions Kevin. Seu filho vai adorar o boneco do filme Meu Malvado Favorito, com braços articulados e cintura giratória. A diversão estará garantida.",
          :valorunitario => 68.90 )
        Produto.create(
-       	 :codigogrupo_id => 1,
          :codigoproduto_id => 4,
          :nomeproduto => "Bonecos Minions Stuart e Kevin",
          :descricaoproduto => "Bonecos de Vinil Minions Stuart e Kevin. Seu filho vai adorar o boneco do filme Meu Malvado Favorito, com braços articulados e cintura giratória. A diversão estará garantida.",
          :valorunitario => 120.90 )
        Produto.create(
-       	 :codigogrupo_id => 1,
          :codigoproduto_id => 5,
          :nomeproduto => "Bonecos Minions Stuart e Bob",
          :descricaoproduto => "Bonecos de Vinil Minions Stuart e Bob. Seu filho vai adorar o boneco do filme Meu Malvado Favorito, com braços articulados e cintura giratória. A diversão estará garantida.",
          :valorunitario => 120.90 )
        Produto.create(
-       	 :codigogrupo_id => 1,
          :codigoproduto_id => 6,
          :nomeproduto => "Bonecos Minions Stuart, Bob e Kevin",
          :descricaoproduto => "Bonecos de Vinil Minions Stuart, Bob e Kevin. Seu filho vai adorar o boneco do filme Meu Malvado Favorito, com braços articulados e cintura giratória. A diversão estará garantida.",
@@ -41,5 +37,6 @@ class AddProdutoData < ActiveRecord::Migration[5.2]
 
   def self.down
         Produto.delete_all
+        Grupo.delete_all
   end
 end
