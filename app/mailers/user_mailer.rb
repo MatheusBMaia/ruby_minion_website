737ee408ab617333e7a5ddefabf6e6b8bbe3f48d
MatheusBMaia <matt.485@hotmail.com>
2019-04-05 23:51:35 -0300

    heroku commit

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -1,9 +1,3 @@
 class UserMailer < ApplicationMailer
-  default from: 'rubytestrails@outlook.com'
 
-  def welcome_email(user)
-  	@user = user 
-  	@url = 'http://example.com/login'
-  	mail(to: @user.email, subject:  'Realizada Reserva de Produtos.'
-  end
 end
