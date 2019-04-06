e4f2e51a84efb146bdacb5860ceb8cec931046ec
MatheusBMaia <matt.485@hotmail.com>
2019-04-06 00:41:43 -0300

    heroku commit

--- app/models/grupo.rb
+++ app/models/grupo.rb
@@ -1,3 +1,2 @@
 class Grupo < ApplicationRecord
-	has_many :produtos
 end
--- app/models/produto.rb
+++ app/models/produto.rb
@@ -1,9 +1,4 @@
 class Produto < ApplicationRecord
-  belongs_to :codigogrupo
-
-  attr_accessor :new_grupo_nomegrupo
-
-  before_save :create_grupo_from_nomegrupo
 
   has_attached_file :imagem, :styles => { :thumb => "66x100", :cover => "132x200" }, :url => "/images/imagem/:id-:style.:extension", :default_url => "/images/imagem.png"
 
@@ -13,9 +8,5 @@ class Produto < ApplicationRecord
   validates_numericality_of :valorunitario
   validates_attachment_size :imagem, :less_than => 500.kilobytes
   validates_attachment_content_type :imagem, :content_type => ['image/jpeg', 'image/pjpeg', 'image/png', 'image/x-png', 'image/gif']
- 
 
-  def create_grupo_from_nomegrupo
-    create_grupo(:nomegrupo => new_grupo_nomegrupo) 
-  end
 end
