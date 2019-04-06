b9ad178a7a123b1a4553798c24edbfcbb25aa2fd
MatheusBMaia <matt.485@hotmail.com>
2019-04-06 00:45:41 -0300

    heroku commit

--- app/models/produto.rb
+++ app/models/produto.rb
@@ -2,11 +2,4 @@ class Produto < ApplicationRecord
 
   has_attached_file :imagem, :styles => { :thumb => "66x100", :cover => "132x200" }, :url => "/images/imagem/:id-:style.:extension", :default_url => "/images/imagem.png"
 
-  validates_numericality_of :codigoproduto  
-  validates_presence_of :nomeproduto
-  validates_presence_of :descricaoproduto
-  validates_numericality_of :valorunitario
-  validates_attachment_size :imagem, :less_than => 500.kilobytes
-  validates_attachment_content_type :imagem, :content_type => ['image/jpeg', 'image/pjpeg', 'image/png', 'image/x-png', 'image/gif']
-
 end
